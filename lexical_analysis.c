034439e9203a555485ff86efc06ac7f8a1defdd7
Karan Agrawal <72506453+EagerBeagle@users.noreply.github.com>
2024-02-25 15:56:08 +0530

    driver

---
+++ lexical_analysis.c
@@ -0,0 +1,43 @@
+#include "lexical_analysis.h"
+
+void lexical_analysis()
+{
+    info("testing LEXER");
+    // Lexer lexer = init_lexer("test.txt");
+    // Lexer lexer = init_lexer("test/test_char.txt");
+    Lexer lexer = init_lexer("Test Cases/t1.txt");
+
+    Token tk = getNextToken(lexer);
+    // printf("fwd0 %d \n", lexer->fwd_ptr);
+
+    Vector vec = init_vector(TOKEN);
+    // push_back(vec, tk1);
+
+    // Token tk = NULL;
+    printf("%s \n", lexer->buff1);
+    while ((lexer->fp))
+    {
+        // if(tk1_read)
+        // printf("%s\n", token_type_list[tk->type]);
+        push_back(vec, tk);
+        tk = getNextToken(lexer);
+    }
+
+    for (int i = 0; i < vec->size; i++)
+    {
+        printf("Line No. %ld  ", ((Token)get(vec, i))->line_num);
+        if (((Token)get(vec, i))->type == 57)
+        {
+            printf("Error: %s", ((Token)get(vec, i))->error_msg);
+        }
+        else
+        {
+            printf("Lexeme %s       ", (char *)(((Token)get(vec, i))->lexeme_str)->text);
+            printf("Token %s", token_type_list[((Token)get(vec, i))->type]);
+        }
+
+        printf("\n");
+    }
+
+    success("LEXER tested successfully.");
+}
---
+++ lexical_analysis.h
@@ -0,0 +1,8 @@
+#ifndef LEXER_TEST
+#define LEXER_TEST
+
+#include "whole_include.h"
+
+void lexical_analysis();
+
+#endif
