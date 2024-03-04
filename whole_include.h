a43498c86fde318fa090526b0864eb1de341dc48
Karan Agrawal <72506453+EagerBeagle@users.noreply.github.com>
2024-03-05 01:24:13 +0530

    group names commented

--- whole_include.h
+++ whole_include.h
@@ -1,3 +1,11 @@
+/* 
+Group Number: 28
+1. Anishka Singh 2020B3A70816P
+2. Gautam Jajoo 2020B3A71638P
+3. Suraj Phalod 2020B3A71959P
+4. Ria Shekhawat 2020B4A71986P
+5. Karan Agrawal 2020B4A70830P
+*/ 
 #ifndef WHOLE
 #define WHOLE
 
